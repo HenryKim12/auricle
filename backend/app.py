c5a5418d3d90557950646a47e48a4a14c213caeb
David <dgmurgulet@gmail.com>
2025-01-18 21:59:48 -0800

    socket changes in app.py + auricle.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from services.auricle import auricleService
 from services.user import userService
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 # Register the blueprints
 app.register_blueprint(auricleService, url_prefix='/auricle')
@@ -13,4 +15,5 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == "__main__":
+    socketio.run(app, debug=True)
     app.run(debug=True)
